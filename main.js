eab001d1a21efd7aa0b0dfd737c0048c9ec83760
Mikaela Matthews <mikaelamatthews@Mikaelas-MacBook-Air.local>
2026-01-05 01:44:20 -0600

    added numQuestion input to local storage savebtn is pressed

--- main.js
+++ main.js
@@ -1,2 +1,34 @@
 /* have number input set to only numbers
-store number in local storage when save button is pressed*/
+store number input in local storage when save button is pressed
+save checked subjects in local storage
+remove if unchecked
+
+if page is refreshed, show user settings from local storage"
+*/
+
+const numQuestions = document.getElementById('numQuestions')
+const subjectBox = document.querySelectorAll('subjectBox')
+const submitBtn = document.getElementById('submitBtn')
+
+numQuestions.addEventListener('input', numberInputStorage)
+
+function numberInputStorage(){
+	const regex = /^[^\d]+$/
+	numQuestions.value = numQuestions.value.replace(regex, "")
+
+	const numberNameAttr = numQuestions.getAttribute('name')
+
+	if (numQuestions.value !== ''){
+		localStorage.setItem(numberNameAttr, numQuestions.value)
+	}
+
+	else {
+		localStorage.removeItem(numberNameAttr)
+	}
+}
+
+submitBtn.addEventListener('click', (ev) => {
+	ev.preventDefault()
+	numberInputStorage()
+
+})
